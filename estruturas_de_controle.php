6d97b7ae9e8a6ca0af13be91bb29c9bc42cf11c3
Marcel Sussai <marcelsussaideoliveira@hotmail.com>
2017-11-08 16:49:35 -0200

    add informações

--- estruturas_de_controle.php
+++ estruturas_de_controle.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'var_util.php';
-// -If--------------------------------------------┐
+// -If----------------                    ----------------------------┐
     // ┌--------------------------------------------------------------┐
     // |    if                                                        |
     // | A resposta da idade                                          |
@@ -33,12 +33,74 @@ require_once 'var_util.php';
         else {
             echo "não é um valor válido!";
         }
+        echo $pl;
     // | comparando com o if shortcut ou seja, uma maneira curta de   |
     // | validar coisas simples como a escrita de um texto            |
     // | chamado de operador ternário                                 |
     // | Exemplo:                                                     |
-        echo ($idadeR < $idadeA)?"Menor de idade":"Maior de idade";
+        echo ($idadeR < $idadeA)?"Menor de idade":"Maior de idade".$pl.$hr;
     // └--------------------------------------------------------------┘
 // -------------------------------------------------------------------┘
+
+// -Switch Case-------------------------------------------------------┐
+    // ┌--------------------------------------------------------------┐
+    // |    Switch Case                                               |
+        $diaSemana = date("w");
+    // | caso seja o especificado executa o comando antes do break    |
+    // | exite função nativa php para tratar datas, mas é apenas      |
+    // | para fins didáticos:                                          |
+        switch ($diaSemana) {
+            case 0:
+                echo "domingo";
+            break;
+            case 1:
+                echo "segunda";
+            break;
+            case 2:
+                echo "terça";
+            break;
+            case 3:
+                echo "quarta";
+            break;
+            case 4:
+                echo "quinta";
+            break;
+            case 5:
+                echo "sexta";
+            break;
+            case 6:
+                echo "sábado";
+            break;
+            default: echo "data inválida";
+        }
+        echo $pl.$hr;
+    // └--------------------------------------------------------------┘
+// -------------------------------------------------------------------┘
+
+
+// -For---------------------------------------------------------------┐
+    // ┌--------------------------------------------------------------┐
+    // |    For                                                       |
+    // | Cuidado com os loops infinitos!!!                            |
+    // | Primeira parte: Cria o contador                              |
+    // | Segunda parte: enquanto o contador for menor que 10 rode     |
+    // | Terceira parte: incrementa o contador                        |
+        for ($i = 0; $i < 10; $i++) {
+            echo $i." ";
+        }
+        echo $pl.$pl;
+    // | para imprimir de 5 em 5 até 500                              |
+        for ($i = 0; $i <= 1000; $i+=5) {
+            echo $i." ";
+        }
+        echo $pl.$pl;
+    // └--------------------------------------------------------------┘
+// -------------------------------------------------------------------┘
+
+
+
+
+
+
 
 ?>
