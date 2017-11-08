8d79daac2a5650ddc9d10bc56f68ea159ec803f0
Marcel Sussai <marcelsussaideoliveira@hotmail.com>
2017-11-08 13:20:23 -0200

    Mais modularização

---
+++ strings.php
@@ -0,0 +1,20 @@
+<?php 
+include 'var_util.php';
+
+
+// -------------------------------------------------------------------┐
+    // ┌--------------------------------------------------------------┐
+    // |    Strings                                                   |
+        $aspasDuplas = "Aspas Duplas";
+        $aspasSimples = 'Aspas Simples!';
+        echo $pr1;
+        echo var_dump($aspasDuplas, $aspasSimples);
+        echo $pr2.$pl;
+        echo "Este é o texto com $aspasDuplas".$pl;
+        
+
+
+         
+	// └--------------------------------------------------------------┘
+// -------------------------------------------------------------------┘
+?>
