cd335c10723316fef532375ca920a696e90aa633
Marcel Sussai <marcelsussaideoliveira@hotmail.com>
2017-11-10 17:54:38 -0200

    adições

--- source/funcoes.php
+++ source/funcoes.php
@@ -8,7 +8,7 @@ require_once 'var_util.php';
         function nomeFunc(){
             return "Conteúdo da função";
         }
-    // | Chamando uma função na variável                                          |
+    // | Chamando uma função na variável                              |
         $resFunc = nomeFunc();
         echo $resFunc.$pl;
     // | Direto no echo                                               |
@@ -17,4 +17,26 @@ require_once 'var_util.php';
     // └--------------------------------------------------------------┘
 // -------------------------------------------------------------------┘
 
+// -Parametros de funções---------------------------------------------┐
+    // ┌--------------------------------------------------------------┐
+    // |    Parametros de funções                                     |
+    // | Exemplo de função com parâmetros                             |
+    // | Parâmetros sem valor padrão ponha primeiro, a esquerda       |
+    // | os que tiveram valores padrões não sendo obrigatório         |
+    // | coloque-os por último a direita                              |
+        function respondeNome($nome, $sobrenome, $idade = 0) {
+            global $pl, $hr, $s1, $s2;
+            echo 'Seu nome é '.$s1.$nome.$s2.' e seu sobrenome '
+                 .$s1.$sobrenome.$s2.$pl;
+            echo 'Sua idade é '.$s1.$idade.$s2.$pl;
+            echo ($idade > 18)?$s1.'você é de maior'.$s2:$s1
+                 .'você é de menor'.$s2;
+            echo $hr;
+        }
+    // | Utilizando a função                                          |
+        echo respondeNome('Marcel', 'Sussai', 31);
+        echo respondeNome('Gláucia', 'Mariane Rosa', 28);
+        echo respondeNome('NomeTeste', 'SObrenomeTeste');
+    // └--------------------------------------------------------------┘
+// -------------------------------------------------------------------┘
 ?>
