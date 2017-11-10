da3a9e61920caba6b546863126ef0268bd516af9
Marcel Sussai <marcelsussaideoliveira@hotmail.com>
2017-11-10 03:06:02 -0200

    mais um comit com uma mensagem sem sentido

---
+++ source/sessoes.php
@@ -0,0 +1,15 @@
+<?php
+// -------------------------------------------------------------------┐
+    // ┌--------------------------------------------------------------┐
+    // |    Sessões                                                   |
+    // | O que são sessões                                            |
+    // | variável local funciona com include no arquivo, ou na pagina |
+    // | variavel de sessão se assemelham a SUPER_GLOBAIS             |
+    // | funciona durante uma conexão com um site                     |
+    // | ele cria um arquivo de texto temporário no cliente e         |
+    // | no servidor registrando informações                          |
+    // | A sessão por default dura 20min mas é configurável           |
+    // | e podemos matalá também                                      |
+    // └--------------------------------------------------------------┘
+// -------------------------------------------------------------------┘
+?>
