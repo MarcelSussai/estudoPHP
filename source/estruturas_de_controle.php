762dd5a1b508b498b27c84f2faeaaa227d7df307
Marcel Sussai <marcelsussaideoliveira@hotmail.com>
2017-11-15 15:27:37 -0200

    mais alteração

--- source/estruturas_de_controle.php
+++ source/estruturas_de_controle.php
@@ -1,91 +1,90 @@
 <?php
 require_once './var_util.php';
-// -If----------------------------------------------------------------┐
-    // ┌--------------------------------------------------------------┐
-    // |    if                                                        |
-    // | A resposta da idade                                          |
-        $idadeR = 31;
-    // |--------------------------------------------------------------|
-    // | Variáveis de comparação                                      |
-        $idadeC = 12;
-        $idadeA = 18;
-        $idadeI = 60;
-    // |--------------------------------------------------------------|
-    // | Realizando o if()                                            |
-    // | se a resposta da idade for menor ou igual a idade da criança |
-    // | exiba "Criança"                                              |
-        if ($idadeR <= $idadeC) {
-            echo "Criança";
-        }
-    // |--------------------------------------------------------------|
-    // | Senão se idade for maior que cirança e menor que adulto      |
-    // | Então exiba "Adolescente"                                    |
-        else if ($idadeR > $idadeC && $idadeR < $idadeA) {
-            echo "Adolescente";
-        }
-    // |--------------------------------------------------------------|
-    // | Senão se idade for maior ou igual a adulto e menor que idoso |
-    // | exiba Adulto
-        else if ($idadeR >= $idadeA && $idadeR < $idadeI) {
-            echo "Adulto";
-        } 
-    // |--------------------------------------------------------------|
-    // | senão se idade for maior ou igual a idoso exiba "Idoso       |
-        else if ($idadeR >= $idadeI) {
-            echo "Idoso";
-        }
-    // |--------------------------------------------------------------|
-    // | senão exiba: "não é um valor válido!"                        | 
-        else {
-            echo "não é um valor válido!";
-        }
-        echo $pl;
-    // |--------------------------------------------------------------|
-    // | comparando com o if shortcut ou seja, uma maneira curta de   |
-    // | validar coisas simples como a escrita de um texto            |
-    // | chamado de operador ternário                                 |
-    // | Exemplo:                                                     |
-        echo ($idadeR < $idadeA)?"Menor de idade":"Maior de idade".$hr;
-    // └--------------------------------------------------------------┘
-// -------------------------------------------------------------------┘
+// ────────────────────────────────────────────────────────────────────────────────────────────────┐
+// If
+  // ┌─────────────────────────────────────────────────────────────────────────────────────────────┐
+  //    Variáveis
+    $idadeR = 31;
+    $idadeC = 12;
+    $idadeA = 18;
+    $idadeI = 60;
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  /*    Realizando o if
+   *    se a resposta da idade for menor ou igual a idade da criança exiba "Criança" 
+   */
+    if ($idadeR <= $idadeC) {
+        echo "Criança";
+    }
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  /*    Senão se idade for maior que cirança e menor que adulto Então exiba "Adolescente"
+   */
+    else if ($idadeR > $idadeC && $idadeR < $idadeA) {
+        echo "Adolescente";
+    }
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  /*    Senão se idade for maior ou igual a adulto e menor que idoso exiba Adulto
+   */
+    else if ($idadeR >= $idadeA && $idadeR < $idadeI) {
+        echo "Adulto";
+    } 
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  /*    senão se idade for maior ou igual a idoso exiba "Idoso"
+   */
+    else if ($idadeR >= $idadeI) {
+        echo "Idoso";
+    }
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  /*    senão exiba: "não é um valor válido!"
+   */
+    else {
+        echo "não é um valor válido!";
+    }
+    echo $pl;
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  /*    comparando com o if shortcut ou seja, uma maneira curta de validar coisas simples como 
+   *    a escrita de um texto chamado de operador ternário
+   *    Exemplo:
+   */
+    echo ($idadeR < $idadeA)?"Menor de idade":"Maior de idade".$hr;
+  // └─────────────────────────────────────────────────────────────────────────────────────────────┘
+// ────────────────────────────────────────────────────────────────────────────────────────────────┘
 
-
-// -Switch Case-------------------------------------------------------┐
-    // ┌--------------------------------------------------------------┐
-    // |    Switch Case                                               |
-    // | Criando a variável                                           |
-        $diaSemana = date("w");
-    // |--------------------------------------------------------------|
-    // | caso seja o especificado executa o comando antes do break    |
-    // | existe função nativa php para tratar datas, mas é apenas     |
-    // | para fins didáticos:                                         |
-        switch ($diaSemana) {
-            case 0:
-                echo "domingo";
-            break;
-            case 1:
-                echo "segunda";
-            break;
-            case 2:
-                echo "terça";
-            break;
-            case 3:
-                echo "quarta";
-            break;
-            case 4:
-                echo "quinta";
-            break;
-            case 5:
-                echo "sexta";
-            break;
-            case 6:
-                echo "sábado";
-            break;
-            default: echo "data inválida";
-        }
-        echo $pl.$hr;
-    // └--------------------------------------------------------------┘
-// -------------------------------------------------------------------┘
+// ────────────────────────────────────────────────────────────────────────────────────────────────┐
+// Switch Case
+  // ┌─────────────────────────────────────────────────────────────────────────────────────────────┐
+  // Criando a variável
+    $diaSemana = date("w");
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  /*    caso seja o especificado executa o comando antes do break existe função nativa php
+   *     para tratar datas, mas é apenas para fins didáticos:
+   */
+    switch ($diaSemana) {
+        case 0:
+            echo "domingo";
+        break;
+        case 1:
+            echo "segunda";
+        break;
+        case 2:
+            echo "terça";
+        break;
+        case 3:
+            echo "quarta";
+        break;
+        case 4:
+            echo "quinta";
+        break;
+        case 5:
+            echo "sexta";
+        break;
+        case 6:
+            echo "sábado";
+        break;
+        default: echo "data inválida";
+    }
+    echo $pl.$hr;
+  // └─────────────────────────────────────────────────────────────────────────────────────────────┘
+// ────────────────────────────────────────────────────────────────────────────────────────────────┘
 
 
 // -For---------------------------------------------------------------┐
