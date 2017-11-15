ca825bd9886ea4812c84b3777e988a1318a13be8
Marcel Sussai <marcelsussaideoliveira@hotmail.com>
2017-11-15 19:43:57 -0200

    refatorando comentários

--- index.php
+++ index.php
@@ -1,71 +1,65 @@
 <?php
+// ────────────────────────────────────────────────────────────────────────────────────────────────┐
+// Includes
+  // ┌─────────────────────────────────────────────────────────────────────────────────────────────┐
+  //                        Includes e Requires
+  /*    Includes incluem arquivos tornando a aplicação modularizada, ou seja, você pode juntar
+   *    arquivos php para formar um conteúdo que pode ser reútilizável mas com apenas um código
+   *    em um arquivo separado.
+   * 
+   *                        Diferença Entre Require e Include
+   *    require : não funciona se o arquivo não existir ou se tiver algum problema.
+   *    include : tenta executar mesmo assim, além de ter mais recursos como include path que pode
+   *              trazer os arquivos na pasta especificada e se não existir ele trás da raiz.
+   *    Exemplos de includes
+   */
+    // include 'source/somar.php';
+    // echo somar(2,14).$pl;
+    // echo somar(2,6).$pl;
+    // echo somar(200,56).$pl;
+  // └─────────────────────────────────────────────────────────────────────────────────────────────┘
+// ────────────────────────────────────────────────────────────────────────────────────────────────┘
 
-// -Começo------------------------------------------------------------┐
-    // ┌--------------------------------------------------------------┐
-    // |    Variaveis úteis                                           |
-        require_once 'var_util.php';
-    // |--------------------------------------------------------------|
-    // | Cabeçalho, começo da página                                  |
-        include_once 'source/head.php';
-    // |--------------------------------------------------------------|
-    // | Antes, um olá mundo .... :)                                  |
-    // |    Olá Mundo!                                                |
-        echo "Hello World - Olá Mundo ! Estudo!".$pl;
-        echo "Os comentários e a forma em que o código é identado
-              é apenas para fins didáticos, em produção utilizo outra
-              estrutura de comentário/código/resultado";
-        echo $hr;
-    // └--------------------------------------------------------------┘
-// -------------------------------------------------------------------┘
+// ────────────────────────────────────────────────────────────────────────────────────────────────┐
+// Começo! Indice!
 
+  // ┌─────────────────────────────────────────────────────────────────────────────────────────────┐
+  //    Requires Necessários!  (Arquivos externos requeridos)
+  /*    Variáveis úteis!    
+   *
+   */ 
+    require_once 'var_util.php';
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  //    Cabeçalho, começo da página
+    include_once 'source/head.php';
+  // └─────────────────────────────────────────────────────────────────────────────────────────────┘
 
-// -INCLUDES----------------------------------------------------------┐
-    // ┌--------------------------------------------------------------┐
-    // |    Includes e Requires                                       |
-    // | Includes incluem arquivos tornando a aplicação modularizada, |
-    // | ou seja, você pode juntar arquivos php para formar um        |
-    // | conteúdo que pode ser reútilizável mas com apenas um código  |
-    // | em um arquivo separado                                       |
-    // | Diferença entre require e include                            |
-    // | require : não funciona se o arquivo não existir ou se tiver  |
-    // | algum problema, já o include ele tenta executar mesmo assim  |
-    // | além de ter mais recursos como include path que ele pode     |
-    // | trazer os arquivos na pasta especificada e se não existir    |
-    // | ele trás da raiz                                             |
-    // | mas o require é mais seguro                                  |
-    // └--------------------------------------------------------------┘
+  // ┌─────────────────────────────────────────────────────────────────────────────────────────────┐
+  //    Olá mundo!
+    echo "Hello World - Olá Mundo ! Estudo!".$pl;
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  //    Observação
+        echo "Os comentários e a forma em que o código é identado é apenas para fins didáticos, 
+              em produção utilizo outra estrutura de comentário/código/resultado".$hr;
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  //    Arquivos / Conteúdo
+    // require 'source/variaveis.php';
+    // include 'source/operadores.php';
+    // include 'source/strings.php';
+    // include 'source/estruturas_de_controle.php';
+    // include 'source/arrays.php';
+    // include 'source/constantes.php';
+    // include 'source/sessoes.php';
+    // include 'source/funcoes.php';
+    // include 'source/datas.php';
+    // include 'source/POO/OrientaObjeto.php';
+    include 'source/POO/Controlador/Definicao.php';
+  // └─────────────────────────────────────────────────────────────────────────────────────────────┘
+// ────────────────────────────────────────────────────────────────────────────────────────────────┘
 
-    // ┌--------------------------------------------------------------┐
-    // |    Exemplos de includes                                      |
-        // include 'source/somar.php';
-        // echo somar(2,14).$pl;
-        // echo somar(2,6).$pl;
-        // echo somar(200,56).$pl;
-    // └--------------------------------------------------------------┘
-// -------------------------------------------------------------------┘
-
-
-// -Arquivos Conteúdo-------------------------------------------------┐
-    // ┌--------------------------------------------------------------┐
-    // |    Arquivos                                                  |
-        // require 'source/variaveis.php';
-        // include 'source/operadores.php';
-        // include 'source/strings.php';
-        // include 'source/estruturas_de_controle.php';
-        // include 'source/arrays.php';
-        // include 'source/constantes.php';
-        // include 'source/sessoes.php';
-        // include 'source/funcoes.php';
-        // include 'source/datas.php';
-        // include 'source/POO/OrientaObjeto.php';
-        include 'source/POO/Controlador/Definicao.php';
-    // └--------------------------------------------------------------┘
-// -------------------------------------------------------------------┘
-
-// -Final-------------------------------------------------------------┐
-    // ┌--------------------------------------------------------------┐
-    // |    Final da Página                                           |
-        finalPagina();
-    // └--------------------------------------------------------------┘
-// -------------------------------------------------------------------┘
+// ────────────────────────────────────────────────────────────────────────────────────────────────┐
+//  Final
+    finalPagina();
+// ────────────────────────────────────────────────────────────────────────────────────────────────┘
+    
 ?>
