4594946b75134feca482d77ec9eb37b956dac9a0
Marcel Sussai <marcelsussaideoliveira@hotmail.com>
2017-11-18 06:07:09 -0200

    data:18/nov2017-06:07

--- source/POO/OrientaObjeto.php
+++ source/POO/OrientaObjeto.php
@@ -1,99 +1,91 @@
 <?php
 require_once './var_util.php';
+// ────────────────────────────────────────────────────────────────────────────────────────────────┐
+//  Orientação a objetos
+  // ┌─────────────────────────────────────────────────────────────────────────────────────────────┐
+  /*    Orientação a Objetos no PHP
+   *
+   *    Objetos são classes que podem ser instanciadas e usadas de diversas maneiras, e classes
+   *    são blocos de códigos que são inteligentes e reútilizáveis 
+   *    Tambem podem ser entendidos objetos, como as variáveis que são instâncias de uma classe
+   *    Um objeto possui caracteristicas, comportamentos e estados
+   *
+   *    Visibilidade de um Objeto
+   *        (+) Publico → A classe atual e todas as outras classes
+   *        (-) Privado → Somente a classe atual
+   *        (#) Protegido → A classe atual e todas suas subclasses
+   *    
 
-// -Orientação a objetos----------------------------------------------┐
-    // ┌--------------------------------------------------------------┐
-    // |    Orientação a Objetos no PHP                               |
-    // | Objetos são classes que podem ser instanciadas e usadas de   |
-    // | diversas maneiras, e classes são blocos de códigos que       |
-    // | são inteligentes e reútilizáveis                             |
-    // | Tambem podem ser entendidos objetos como as variáveis que    |
-    // | são instâncias de uma classe                                 |
-    // | Um objeto possui caracteristicas, comportamentos e estados   |
-    // |--------------------------------------------------------------|
-    // |    Visibilidade de um Objeto                                 |
-    // | (+)    Publico                                               |
-    // |            A classe atual e todas as outras classes          |
-    // | (-)    Privado                                               |
-    // |            Somente a classe atual                            |
-    // | (#)    Protegido                                             |
-    // |            A classe atual e todas suas subclasses            |
-    // |--------------------------------------------------------------|
-    // | Diagrama de classes                                          |
-    // ┌======================================┐                       |
-    // |    Caneta                            |                       |
-    // |======================================|                       |
-    // |  (+)   modelo                        |                       |
-    // |  (+)   cor                           |                       |
-    // |  (-)   ponta                         |                       |
-    // |  (#)   carga                         |                       |
-    // |  (#)   tampada                       |                       |
-    // |======================================|                       |
-    // |  (+)   escrever()                    |                       |
-    // |  (+)   rabiscar()                    |                       |
-    // |  (+)   pintar()                      |                       |
-    // |  (+)   tampar()                      |                       |
-    // |  (+)   destampar()                   |                       |
-    // └======================================┘                       |
-    // |--------------------------------------------------------------|
-    // | Exemplo de um objeto                                         |
-    // | Caneta                                                       |
-    // |--------------------------------------------------------------|
-    // | ATRIBUTOS - Coisas que tem: (Características)                |
-    // |    Modelo      | caractere         | string                  |
-    // |    Cor         | caractere         | string                  |
-    // |    Ponta       | real              | float                   |
-    // |    Carga       | inteiro           | int                     |
-    // |    Tampada     | lógico            | boolean                 |
-    // |--------------------------------------------------------------|
-    // | MÉTODOS - Coisas que faz: (comportamentos)                   |
-    // |    Escreve                                                   |
-    // |    Rabisca                                                   |
-    // |        se tampada então                                      |
-    // |            ERRO                                              |
-    // |        senão rabisca                                         |
-    // |    Pinta                                                     |
-    // |    Tampa                                                     |
-    // |    Destampa                                                  |
-    // |--------------------------------------------------------------|
-    // | ESTADO - Como está (status)                                  |
-    // |    Se está tampada ou não                                    |
-    // |    Se está escrevendo ou não                                 |
-    // |    a carga ou qualquer estado de caracteristicas             |
-    // |    mutáveis                                                  |
-    // |--------------------------------------------------------------|
-    // | Métodos especiais                                            |
-    // |    Métodos Acessores (getters)                               |
-    // |    Métodos Modificadores (setters)                           |
-    // |    Métodos Construtores ()                                   |
-    // |--------------------------------------------------------------|
-    // | requerindo a classe caneta                                   |
-        require_once 'caneta.php';
-    // | Criando o Objeto caneta                                      |
-        $c1 = new Caneta("BIC",0.5);
-    // |--------------------------------------------------------------|
-    // | Exibindo o objeto                                            |
-        mostrarEstado($c1);
-    // |--------------------------------------------------------------|
-    // | Utilizando um método da classe                               |
-        echo $c1->escrever("Estou escrevendo").$pl;
-        $c1->destampar();
-        echo $c1->escrever("Agora Estou escrevendo").$hr;
-    // |--------------------------------------------------------------|
-    // | Criando outra instancia, outro objeto                        |
-        $c2 = new Caneta("COMPACTOR", 1.0,"verde");
-    // |--------------------------------------------------------------|
-    // | Exibindo o objeto                                            |
-        mostrarEstado($c2);
-    // |--------------------------------------------------------------|
-    // | Rabiscando com o objeto caneta                               |
-        $c2->destampar();
-        $c2->rabiscar(16);
-    // |--------------------------------------------------------------|
-
-
-
-
-    // └--------------------------------------------------------------┘
-// -------------------------------------------------------------------┘
-
+   *   Diagrama de classes
+   *  ======================================┐
+   *      Caneta                            |
+   *  ======================================|
+   *    (+)   modelo                        |
+   *    (+)   cor                           |
+   *    (-)   ponta                         |
+   *    (#)   carga                         |
+   *    (#)   tampada                       |
+   *  ======================================|
+   *    (+)   escrever()                    |
+   *    (+)   rabiscar()                    |
+   *    (+)   pintar()                      |
+   *    (+)   tampar()                      |
+   *    (+)   destampar()                   |
+   *  ======================================┘
+   *
+   *   Exemplo de um objeto
+   *   Caneta
+   *
+   *   ATRIBUTOS - Coisas que tem: (Características)
+   *      Modelo      | caractere         | string
+   *      Cor         | caractere         | string
+   *      Ponta       | real              | float
+   *      Carga       | inteiro           | int
+   *      Tampada     | lógico            | boolean
+   *
+   *   MÉTODOS - Coisas que faz: (comportamentos)
+   *      Escreve
+   *      Rabisca
+   *          se tampada então
+   *              ERRO
+   *          senão rabisca
+   *      Pinta
+   *      Tampa
+   *      Destampa
+   *
+   *   ESTADO - Como está (status)
+   *      Se está tampada ou não
+   *      Se está escrevendo ou não
+   *      a carga ou qualquer estado de caracteristicas
+   *      mutáveis
+   *
+   *   Métodos especiais   
+   *      Métodos Acessores (getters)
+   *      Métodos Modificadores (setters)
+   *      Métodos Construtores ()
+   */
+  //    Requerindo a classe caneta
+    require_once 'caneta.php';
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  //    Criando o Objeto caneta
+    $c1 = new Caneta("BIC",0.5);
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  //    Exibindo o objeto
+    mostrarEstado($c1);
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  //    Utilizando um método da classe
+    echo $c1->escrever("Estou escrevendo").$pl;
+    $c1->destampar();
+    echo $c1->escrever("Agora Estou escrevendo").$hr;
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  //    Criando outra instancia, outro objeto
+    $c2 = new Caneta("COMPACTOR", 1.0,"verde");
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  //    Exibindo o objeto
+    mostrarEstado($c2);
+  // |─────────────────────────────────────────────────────────────────────────────────────────────|
+  //    Rabiscando com o objeto caneta
+    $c2->destampar();
+    $c2->rabiscar(16);
+  // └─────────────────────────────────────────────────────────────────────────────────────────────┘
+// ────────────────────────────────────────────────────────────────────────────────────────────────┘
---
+++ source/POO/Polimorfismo.php
@@ -0,0 +1,117 @@
+<?php
+// ────────────────────────────────────────────────────────────────────────────────────────────────┐
+//  Polimorfismo
+  /*    Permite que o mesmo nome represente vários comportamentos diferentes.
+   *    
+   *    Assinatura do Método
+   *    Quantidade e o tipo dos parâmetros
+   *    NÃO considera o retorno
+   *    
+   *    ex
+   *    1 e 2 são iguais
+   *    3 é diferente de 1 e 2
+   *    4 é diferente de 3 e diferente de 1 e 2
+   *    5 é diferente de 4 de 3 e de 1 e 2
+   * 
+   *    1 - public function calcMedia($n1(float),$n2(float)) : float
+   *    2 - public function calcMedia($v1(float),$v2(float)) : int
+   *    3 - public function calcMedia($bim(int),$n1(float),$n2(float)) : float
+   *    4 - public function calcMedia($n1(float),$n2(float),$n3(float),$n4(float)) : float
+   *    5 - public function calcMedia($medMin(float),$medMax(float),$sit(string),bim(int)) : string
+   * 
+   *    Tipos de polimorfismo
+   *    1 - Polimorfismo de sobreposição
+   *    2 - Polimorfismo de sobrecarga
+   * 
+   *    Polimorfismo de Sobreposição
+   *        Acontece quando substituimos um método de uma superclasse na sua subclasse, usando
+   *        a mesma assinatura.
+   * 
+   *    
+   *    Métodos abstratos:
+   *    ┌───────────────────┬───────────────────┬───────────────────┬───────────────────┐
+   *    │   Mamifero        │   Rétil           │   Peixe           │   Ave             │
+   *    ├───────────────────┼───────────────────┼───────────────────┼───────────────────┤
+   *    │   Correndo        │   rasteja         │   nadando         │   Voando          │
+   *    │   Mamando         │   comendo insetos │   plancton        │   Comendo Frutas  │
+   *    │   Som de Mamifero │   som de reptil   │   não faz som     │   Som de ave      │
+   *    └───────────────────┴───────────────────┴───────────────────┴───────────────────┘
+   * 
+   *    ┌───────────────────────────────────────┐
+   *    │  [abs]Animal                          │
+   *    ├───────────────────────────────────────┤
+   *    │    $peso                              │
+   *    │    $idade                             │
+   *    │    $membros                           │
+   *    │    [abs]locomover()                   │
+   *    │    [abs]alimentar()                   │
+   *    │    [abs]emitirSom()                   │
+   *    └───────────────────────────────────────┘
+   *    ▲
+   *    ║
+   *    ║   ┌───────────────────────────────────┐
+   *    ╠═══┤   Mamifero                        │
+   *    ║   ├───────────────────────────────────┤
+   *    ║   │       $corPelo                    │
+   *    ║   └───────────────────────────────────┘
+   *    ║       ▲
+   *    ║       ║
+   *    ║       ║   ┌───────────────────────────────────┐
+   *    ║       ╠═══┤   Canguru                         │
+   *    ║       ║   ├───────────────────────────────────┤
+   *    ║       ║   │       usarBolsa()                 │
+   *    ║       ║   │       locomover()     saltando    │
+   *    ║       ║   └───────────────────────────────────┘
+   *    ║       ║
+   *    ║       ║   ┌───────────────────────────────────┐
+   *    ║       ╚═══┤   Cachorro                        │
+   *    ║           ├───────────────────────────────────┤
+   *    ║           │       enterrarOsso()              │
+   *    ║           │       abanarRabo()                │
+   *    ║           └───────────────────────────────────┘
+   *    ║
+   *    ║   ┌───────────────────────────────────┐
+   *    ╠═══┤   Réptil                          │
+   *    ║   ├───────────────────────────────────┤
+   *    ║   │       $corEscama                  │
+   *    ║   └───────────────────────────────────┘
+   *    ║       ▲
+   *    ║       ║
+   *    ║       ║   ┌───────────────────────────────────┐
+   *    ║       ╠═══┤   Cobra                           │
+   *    ║       ║   ├───────────────────────────────────┤
+   *    ║       ║   └───────────────────────────────────┘
+   *    ║       ║
+   *    ║       ║   ┌───────────────────────────────────┐
+   *    ║       ╚═══┤   Tartaruga                       │
+   *    ║           ├───────────────────────────────────┤
+   *    ║           │       locomover()     devagar     │
+   *    ║           └───────────────────────────────────┘
+   *    ║
+   *    ║   ┌───────────────────────────────────┐
+   *    ╠═══┤   Peixe                           │
+   *    ║   ├───────────────────────────────────┤
+   *    ║   │       $corEscama                  │
+   *    ║   │       soltarBolha()               │
+   *    ║   └───────────────────────────────────┘
+   *    ║       ▲
+   *    ║       ║
+   *    ║       ║   ┌───────────────────────────────────┐
+   *    ║       ╚═══┤   Goldfish                        │
+   *    ║           ├───────────────────────────────────┤
+   *    ║           └───────────────────────────────────┘
+   *    ║
+   *    ║   ┌───────────────────────────────────┐
+   *    ╚═══┤   Ave                             │
+   *        ├───────────────────────────────────┤
+   *        │       $corPena                    │
+   *        │       fazerNinho()                │
+   *        └───────────────────────────────────┘
+   *            ▲
+   *            ║
+   *            ║   ┌───────────────────────────────────┐
+   *            ╚═══┤   Arara                           │
+   *                ├───────────────────────────────────┤
+   *                └───────────────────────────────────┘
+   * 
+   */
